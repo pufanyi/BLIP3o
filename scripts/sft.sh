1de98bba7f62c69eff15f9b96f38ff0347cc446f
Fanyi Pu <FPU001@e.ntu.edu.sg>
2025-08-26 15:50:24 +0800

    Refactor sft.sh to streamline training script for single-node execution. Update model and diffusion paths, adjust batch sizes, and enhance logging settings. Remove unnecessary environment variable exports and improve overall script clarity.

--- scripts/sft.sh
+++ scripts/sft.sh
@@ -1,44 +1,14 @@
-#!/bin/bash
-#SBATCH --job-name=blip3o    # Job name
-#SBATCH --nodes=4                         # Number of nodes
-#SBATCH --gres=gpu:8                         # Number of GPUs per node
-#SBATCH --time=96:00:00                      # Time limit (hh:mm:ss)
+CUDA_VISIBLE_DEVICES=3
 
-
-
-conda  activate  your env
-
-export WANDB_API_KEY='your wandb key'
-export HF_HOME=/your/hf/home/
-
-# Fix tokenizers parallelism warning
-export TOKENIZERS_PARALLELISM=false
-
-# Fix PyTorch Dynamo graph break warnings
-export TORCHDYNAMO_CAPTURE_SCALAR_OUTPUTS=1
-
-AR_BACKBONE=Your_Pretrain_CKPT
-DIFFUSION=Efficient-Large-Model/SANA1.5_1.6B_1024px_diffusers
-
-LR=5e-5
-RUN_NAME="SFT"
-
-echo "AR_BACKBONE: ${AR_BACKBONE}"
-echo "DIFFUSION: ${DIFFUSION}"
-echo "RUN_NAME: ${RUN_NAME}"
-
-LOCAL_DIR="models/${RUN_NAME}"
-
-
-srun torchrun --nproc_per_node=8  --nnodes=$SLURM_NNODES \
-    --rdzv_id=$SLURM_JOB_ID --rdzv_backend=c10d --rdzv_endpoint=$HOSTNAME:29501 blip3o/train/train.py \
+torchrun --nproc_per_node=1  --nnodes=1 \
+        blip3o/train/train.py \
     --deepspeed scripts/zero1.json \
     --num_image_tokens 65536 \
     --num_scale_tokens 3 \
     --load_embeddings_from_vision True \
-    --model_name_or_path $AR_BACKBONE \
-    --diffusion_name_or_path  DIFFUSION \
-    --version "qwen_1_5" \
+    --model_name_or_path BLIP3o/BLIP3o-NEXT-Pretrain-3B \
+    --diffusion_name_or_path  BLIP3o/BLIP3o-NEXT-Pretrain-3B \
+    --version "qwen3" \
     --dataset_cls 'mix' \
     --dispatch_batches False \
     --mm_vision_select_layer -2 \
@@ -47,21 +17,21 @@ srun torchrun --nproc_per_node=8  --nnodes=$SLURM_NNODES \
     --image_aspect_ratio square \
     --mm_patch_merge_type flat \
     --bf16 True \
-    --run_name $RUN_NAME \
-    --output_dir ${LOCAL_DIR} \
+    --run_name blip3o \
+    --output_dir ./output \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
-    --per_device_eval_batch_size 4 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --save_strategy "steps" \
     --save_steps 1000 \
     --save_total_limit 1 \
-    --learning_rate ${LR} \
+    --learning_rate 1e-5 \
     --weight_decay 0. \
     --warmup_ratio 0.01 \
     --lr_scheduler_type "cosine_with_min_lr" \
     --lr_scheduler_kwargs '{"min_lr":1e-5}' \
-    --logging_steps 5 \
+    --logging_steps 1 \
     --tf32 True \
     --model_max_length 2048 \
     --gradient_checkpointing True \
@@ -70,4 +40,4 @@ srun torchrun --nproc_per_node=8  --nnodes=$SLURM_NNODES \
     --report_to wandb \
     --torch_compile True \
     --torch_compile_backend inductor \
-    --dataloader_drop_last True 
+    --dataloader_drop_last True
